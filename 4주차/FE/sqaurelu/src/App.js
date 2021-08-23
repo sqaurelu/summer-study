0d8d366fb059b81931d8826beb0dca5d3afc1a53
hwajin <sqaurelu@gmail.com>
2021-08-23 16:05:55 +0900

    Revert "로그인/회원가입 페이지 추가"
    This reverts commit ee9c6366bd3ea96fc85eb5a2b3ee91e59e85710b.

--- 4주차/FE/sqaurelu/src/App.js
+++ 4주차/FE/sqaurelu/src/App.js
@@ -1,21 +1,11 @@
 import './App.css';
-import { Route } from 'react-router-dom';
-import Layout from './styles/Layout';
-import { LoginPage, SignUpPage, FeedPage } from './pages';
-import GlobalStyles from './styles/GlobalStyles';
-import MainPage from './pages/Main';
 
 function App() {
-	return (
-		<Layout>
-			<GlobalStyles />
-			<Route path='/' exact={true} component={MainPage} />
-			<Route path='/login' component={LoginPage} />
-			<Route path='/signup' component={SignUpPage} />
-			<Route path='/feed' component={FeedPage} />
-			{/* 이상한 페이지 갔을 때 에러창 */}
-		</Layout>
-	);
+  return (
+    <div>
+      heelllo
+    </div>
+  );
 }
 
 export default App;
