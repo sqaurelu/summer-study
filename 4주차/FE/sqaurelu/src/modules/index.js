42fe365a0cb2c3d9bc42611629db54f225a89113
hwajin <sqaurelu@gmail.com>
2021-09-02 23:40:04 +0900

    필요없는 코드 삭제

--- 4주차/FE/sqaurelu/src/modules/index.js
+++ 4주차/FE/sqaurelu/src/modules/index.js
@@ -1,12 +1,8 @@
 import { combineReducers } from 'redux';
-import login from './login';
-import signup from './signup';
 import post from './post';
 import postList from './postList';
 
 const rootReducer = combineReducers({
-    login,
-    signup,
     post,
     postList,
 });
